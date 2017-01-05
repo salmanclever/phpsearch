91dbf5adf3db940a471ca0e776a8f0e32aa6b736
Salman Jabbari <olddealers@gmail.com>
2017-01-05 12:56:09 +0330

    PM-32 make test for index file

--- test/indexTest.php
+++ test/indexTest.php
@@ -1,34 +1,5 @@
 <?php
-    require_once('../vendor/autoload.php');
-//    require_once('../index.php');
-    use PHPUnit\Framework\TestCase;
 
-
-class Money{
-    public function __construct($amount)
-    {
-        $this->amount = $amount;
-    }
-    public function getAmount()
-    {
-        return $this->amount;
-    }
-    public function negate(){
-        return new Money(-1 * $this->amount);
-    }
-}
-
-
-    class MoneyTest extends TestCase
-    {
-        public function testCanBeNegated(){
-            $word = "juice";
-
-            $result = Module::search($word);
-
-
-        }
-    }
-
-
-?>
+use PHPUnit\Framework\TestCase;
+$salman = new Finder();
+print_r($salman->search('Integer'));
